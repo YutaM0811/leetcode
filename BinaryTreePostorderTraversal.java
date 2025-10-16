2c64896d99902ee3f5a78786f7a67fd2f514fe24
YutaM0811 <ym19980811@gmai.com>
2025-10-16 23:44:50 +0900

    BinaryTreePostorderTraversal

---
+++ BinaryTreePostorderTraversal.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class BinaryTreePostorderTraversal {
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        traverse(root, result);
+        return result;
+    }
+
+    private void traverse(TreeNode node, List<Integer> result) {
+        if (node == null) return;
+
+        traverse(node.left, result);
+        traverse(node.right, result);
+        result.add(node.val);
+    }
+}
