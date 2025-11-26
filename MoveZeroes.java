d9613f47626743d8eb3923f277aea8b14455e502
YutaM0811 <ym19980811@gmai.com>
2025-11-26 22:57:43 +0900

    MoveZeroes

---
+++ MoveZeroes.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class MoveZeroes {
+    public void moveZeroes(int[] nums) {
+        int p = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) nums[p++] = nums[i];
+        }
+
+        while (p < nums.length) {
+            nums[p++] = 0;
+        }
+    }
+}
