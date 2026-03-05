bbe1999d60992861ec2af18d616e22eee432e0b0
YutaM0811 <ym19980811@gmai.com>
2026-03-05 09:33:39 +0900

    MinCostClimbingStairs

---
+++ MinCostClimbingStairs.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class MinCostClimbingStairs {
+    public int minCostClimbingStairs(int[] cost) {
+        int n=0;
+        int dp[]=new int[cost.length];
+        dp[0]=cost[0];
+        dp[1]=cost[1];
+        for(int i=2; i<cost.length; i++) {
+            int m=Math.min(dp[i-1],dp[i-2]);
+            dp[i]=m+cost[i];
+        }
+        return Math.min(dp[cost.length-1],dp[cost.length-2]);
+    }
+}
