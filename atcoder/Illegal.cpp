c29d15eef64e48c26bbde8f0107862c52d8c41f0
YutaM0811 <ym19980811@gmail.com>
2026-04-25 12:17:39 +0900

    Illegal

---
+++ atcoder/Illegal.cpp
@@ -0,0 +1,10 @@
+# include <iostream>
+using namespace std;
+
+int main() {
+    string S;
+    cin>>S;
+    if( S.length()==5||S.length()==10) cout<<"Yes"<<endl;
+    else cout<<"No"<<endl;
+    return 0;
+}
