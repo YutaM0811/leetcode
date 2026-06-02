0f6c03281e0a5ed7b32a8c9ae742a0f33bf642a1
YutaM0811 <ym19980811@gmai.com>
2026-06-02 13:18:42 +0900

    MinStack

---
+++ neetcode/MinStack.cpp
@@ -0,0 +1,30 @@
+# include <bits/stdc++.h>
+using namespace std;
+
+class MinStack {
+public:
+    stack<int> st;
+    stack<int> minSt;
+
+    MinStack() {
+    }
+                
+    void push(int val) {
+        st.push(val);
+        if (minSt.empty() || minSt.top()>val) minSt.push(val);
+        else minSt.push(minSt.top());
+    }
+                    
+    void pop() {
+        st.pop();
+        minSt.pop();
+    }
+                        
+    int top() {
+        return st.top();
+    }
+                            
+    int getMin() {
+        return minSt.top();
+    }
+};
